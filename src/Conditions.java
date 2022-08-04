16a6ecd0eaa4a089e859a54f74b9474efb213425
deepali <neuedatraining19@neueda.com>
2022-08-04 19:00:50 +0000

    changes

--- src/Conditions.java
+++ src/Conditions.java
@@ -6,7 +6,7 @@ public class Conditions {
         if (testScore > 50) {
             System.out.println("Congrats - you passed");
         } else if (testScore == 50) {
-            System.out.println("we are not sure if you passed");
+            System.out.println("we are not sure if you passed!");
         }
           else {
             System.out.println("sorry you failed");
